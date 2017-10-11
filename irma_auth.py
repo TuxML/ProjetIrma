abf531e3fa8ff40f0165385505a5982c6c8e53fc
pleluron <pierre.leluron@gmail.com>
2017-10-11 16:42:11 +0200

    Commande de test

---
+++ irma_auth.py
@@ -0,0 +1,4 @@
+user = {
+	"username":"user",
+	"password":"user"
+}
---
+++ send_config_http.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import http.client
+import json
+import base64
+import irma_auth
+
+try:
+	conn_http = http.client.HTTPConnection("148.60.11.195:8080")
+
+	# Authentification
+	auth_header = {
+		'Content-Type':'application/json',
+		'Accept':'application/json'
+	}
+
+	auth_body = json.dumps(irma_auth.user)
+
+	conn_http.request("POST", "/api/authenticate", auth_body, auth_header)
+	auth_response = conn_http.getresponse()
+	if auth_response.status == 200:
+		auth_id  = json.loads(auth_response.read().decode())['id_token']
+	else:
+		print("Auth failed : " + auth_response.reason)
+		sys.exit(-1)
+
+	headers = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'Authorization': 'Bearer ' + auth_id
+	}
+
+	post_body = json.dumps({
+	  "boot": True,
+	  "boottime": 0,
+	  "compilationtime": 0,
+	  "compile": True,
+	  "configfile": (base64.b64encode(b"ceci est un test")).decode(),
+	  "configfileContentType": "string",
+	  "coresize": 0,
+	  "date": "2017-10-04",
+	  "erreur": (base64.b64encode(b"ceci est un test")).decode(),
+	  "erreurContentType": "string",
+	})
+
+	# Créer une entrée
+	conn_http.request("POST", "/api/i-rma-dbs", post_body, headers)
+	r1 = conn_http.getresponse()
+	print (r1.status, r1.reason, r1.read())
+
+	# Récupérer toutes les entrées
+	conn_http.request("GET", "/api/i-rma-dbs", None, headers)
+	r1 = conn_http.getresponse()
+	print(r1.status, r1.reason, r1.read())
+
+	conn_http.close()
+except http.client.HTTPException as err:
+	if err == http.client.NotConnected: print("Can't connect")
+	else: print(err)
+	sys.exit(-1)
