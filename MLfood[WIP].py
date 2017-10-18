8e541ff590d7cb531382fabb7cf5867d1c14ddb4
Papanevada <alexislm35@hotmail.fr>
2017-10-18 20:41:09 +0200

    Amélioration du script et saisi par l'utilisateur du nombre de compilation a lancer. Commentaires ajoutés

--- MLfood[WIP].py
+++ MLfood[WIP].py
@@ -1,10 +1,21 @@
 import os;
+from sys import argv
 
+
+#Erreur si le nombre de compilation a lancer est vide
+if len(argv) == 1:
+    print("Veuillez préciser un nombre de compilation a lancer.")
+    exit(1)
+
+# On recupere le nombre de compilation a lancer
+nb = int(argv[1])
 
 # Doit contenir la liste des différentes URL d'images systemes contenant le/les scripts tuxml d'execution.
-lurl = ["url1","url2","url3"]
+images = ["url1","url2","url3"]
 
 
-for url in lurl :
-    str = 'docker run -i -t {} /bin/bash'.format(url);
-    system(str);
+# Pour chaque url dans la liste d'url on lance un nouveau docker qui lance la commande TuxML
+for i in range(nb):
+    str = 'docker run -i -t {} /bin/bash'.format(images[i % len(images)])
+    print("\n"+str)
+    os.system(str)
