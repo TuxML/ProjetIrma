d645218bb15c374f468cf8f23de148ef9c04ab40
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2017-10-19 15:54:35 +0200

    Ajout de la gestion pour les gestionnaires de paquets yum/dnf

--- installDependencies.py
+++ installDependencies.py
@@ -6,21 +6,20 @@ import shutil
 def packageManagerIs(packageManagerName):
     return shutil.which(packageManagerName)
 
+def getActualManager(manager1, manager2):
+    if (packageManagerIs(manager1)):
+        return manager1
+    else
+        return manager2
+
+
 def installDependency():
-    if (packageManagerIs("aptitude")):
-        subprocess.run(["aptitude", "update"])
-        returnCode = subprocess.run(["aptitude", "upgrade"]).returncode
+    if (packageManagerIs("aptitude") | packageManagerIs("apt-get")):
+        packageManagerName = getActualManager("aptitude", "apt-get")
+        subprocess.run([packageManagerName, "update"])
+        returnCode = subprocess.run([packageManagerName, "upgrade"]).returncode
         if(returnCode == 0):
-            completedProcess = subprocess.run(["aptitude" , "install" ,"gcc","make",
-            "binutils","util-linux","kmod","e2fsprogs","jfsutils",
-            "reiserfsprogs", "xfsprogs", "squashfs" , "btrfs-progs",
-            "pcmiautils","quotatool", "ppp", "nfs-kernel-server",
-            "procps","grub", "mcelog", "iptables", "openssl", "libcrypto++6","bc"])
-    elif (packageManagerIs("apt-get")):
-        subprocess.run(["apt-get", "update"])
-        returnCode = subprocess.run(["apt-get", "upgrade"]).returncode
-        if(returnCode == 0):
-            completedProcess = subprocess.run(["apt-get" , "install" ,"gcc","make",
+            completedProcess = subprocess.run([packageManagerName, "install" ,"gcc","make",
             "binutils","util-linux","kmod","e2fsprogs","jfsutils",
             "reiserfsprogs", "xfsprogs", "squashfs" , "btrfs-progs",
             "pcmiautils","quotatool", "ppp", "nfs-kernel-server",
@@ -30,6 +29,12 @@ def installDependency():
         if (returnCode == 0):
             completedProcess = subprocess.run(["pacman", "-S", "gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "reiserfsprogs", "xfsprogs", "squashfs-tools",
             "btrfs-progs", "pcmciautils", "quota-tools", "ppp", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile", "grub", "iptables", "openssl", "bc"])
+    elif (packageManagerIs("dnf") | packageManagerIs("yum")):
+        packageManagerName = getActualManager("dnf", "yum")
+        returnCode = subprocess.run([packageManagerName, "upgrade"]).returncode
+        if (returnCode == 0):
+            completedProcess = subprocess.run([packageManagerName, "install", "gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "reiserfs-utils", "xfsprogs", "squashfs-tools",
+            "btrfs-progs", "pcmciautils", "quotatool", "ppp", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile", "grub", "mcelog", "iptables", "openssl", "bc"])
     else:
         returnCode = 1
     return completedProcess.returncode
