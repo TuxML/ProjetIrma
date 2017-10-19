e71a6dcf47c4275a055be6d0f1a813becebbfd09
Alexis LeMasle <14008349@e010m08.istic.univ-rennes1.fr>
2017-10-19 14:39:50 +0200

    gestion de cas d'erreurs

--- MLfood.py
+++ MLfood.py
@@ -1,21 +1,25 @@
-import os;
-from sys import argv
+import os
+import sys
 
 
-#Erreur si le nombre de compilation a lancer est vide
-if len(argv) == 1:
+# Error if there is no argument "number" of compilation to run.
+if len(sys.argv) == 1:
     print("Veuillez préciser un nombre de compilation a lancer.")
     exit(1)
+    
+# Retrieves the number of compilation to run.
+nb = int(sys.argv[1])
 
-# On recupere le nombre de compilation a lancer
-nb = int(argv[1])
-
-# Doit contenir la liste des différentes URL d'images systemes contenant le/les scripts tuxml d'execution.
+# Must contain the list of differents systems images URLs with the execution tuxml script.
 images = ["url1","url2","url3"]
 
+# The image list must not be empty.
+if len(images) == 0:
+	print("Il n'a aucune images système\n")
+	exit(1)
 
-# Pour chaque url dans la liste d'url on lance un nouveau docker qui lance la commande TuxML
+# For each url in the url list "images", we run a new docker which run the TuxML command.
 for i in range(nb):
-    str = 'docker run -i -t {} /bin/bash'.format(images[i % len(images)])
+    str = 'sudo docker run -i -t {} /bin/bash'.format(images[i % len(images)])
     print("\n"+str)
     os.system(str)
