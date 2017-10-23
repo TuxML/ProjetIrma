0e668e8f8fbd84a07174f91cabcb8bd3c98581dc
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2017-10-23 23:55:25 +0200

    refactoring et ajout de la gestion de Gentoo et Suse

--- installDependencies.py
+++ installDependencies.py
@@ -1,42 +1,70 @@
 #!/bin/python
-import platform
 import subprocess
 import shutil
 
+# author : LE FLEM Erwan, MERZOUK Fahim
+# Check if the given package manager is present on the system
+# return true or false
 def packageManagerIs(packageManagerName):
     return shutil.which(packageManagerName)
 
-def getActualManager(manager1, manager2):
-    if (packageManagerIs(manager1)):
-        return manager1
-    else
-        return manager2
+# author : LE FLEM Erwan, MERZOUK Fahim
+# get the package manager present on the system
+# return value :
+#    as a String, the name of the first supported package manager, if any.
+#    None if no supported packages manager has been found e.g pacman.
+def getPackageManager():
+    pkg_managers = ["apt-get", "pacman", "dnf", "yum", "emerge", "zypper"]
+    for manager in pkg_managers:
+        if packageManagerIs(manager):
+            return manager
+    return None
 
-
-def installDependency():
-    if (packageManagerIs("aptitude") | packageManagerIs("apt-get")):
-        packageManagerName = getActualManager("aptitude", "apt-get")
-        subprocess.run([packageManagerName, "update"])
-        returnCode = subprocess.run([packageManagerName, "upgrade"]).returncode
-        if(returnCode == 0):
-            completedProcess = subprocess.run([packageManagerName, "install" ,"gcc","make",
-            "binutils","util-linux","kmod","e2fsprogs","jfsutils",
-            "reiserfsprogs", "xfsprogs", "squashfs" , "btrfs-progs",
-            "pcmiautils","quotatool", "ppp", "nfs-kernel-server",
-            "procps","grub", "mcelog", "iptables", "openssl", "libcrypto++6","bc"])
-    elif (packageManagerIs("pacman")):
-        returnCode = subprocess.run(["pacman", "-Syu"]).returncode
-        if (returnCode == 0):
-            completedProcess = subprocess.run(["pacman", "-S", "gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "reiserfsprogs", "xfsprogs", "squashfs-tools",
-            "btrfs-progs", "pcmciautils", "quota-tools", "ppp", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile", "grub", "iptables", "openssl", "bc"])
-    elif (packageManagerIs("dnf") | packageManagerIs("yum")):
-        packageManagerName = getActualManager("dnf", "yum")
-        returnCode = subprocess.run([packageManagerName, "upgrade"]).returncode
-        if (returnCode == 0):
-            completedProcess = subprocess.run([packageManagerName, "install", "gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "reiserfs-utils", "xfsprogs", "squashfs-tools",
-            "btrfs-progs", "pcmciautils", "quotatool", "ppp", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile", "grub", "mcelog", "iptables", "openssl", "bc"])
+# author : LE FLEM Erwan, MERZOUK Fahim
+# Install the list of given packages
+# return 0 on sucess, -2 if no supported packages manager is found, and other code for other error.
+def installPackages(listOfPackages):
+    managerToCmdInstall = {"apt-get": " -qq -y install ", "pacman": " -S --noconfirm ", "dnf": " -q install ", "yum": " -y install ", "emerge" : " --quiet-build=y -a ", "zypper":" --non-interactive install "}
+    pkg_manager = getPackageManager();
+    if(pkg_manager != None):
+        completedProcess = subprocess.run([pkg_manager + managerToCmdInstall[pkg_manager] + " ".join(listOfPackages)], shell=True)
+        return completedProcess.returncode
     else:
-        returnCode = 1
-    return completedProcess.returncode
+        print("[-] Unsupported package manager")
+        return -2
+
+# author : LE FLEM Erwan, MERZOUK Fahim
+# Install packages of required dependencies to compile the kernel
+#return zero on sucess, =/= on error.
+def installDependency():
+    pkg_manager = getPackageManager();
+
+    if (pkg_manager == None):
+        print("[-] Unsupported package manager")
+        return -2
+    # TODO : check on https://wiki.archlinux.org/index.php/System_maintenance#Partial_upgrades_are_unsupported about issues using -Sy instead of -Syu before installing pkgs.
+    managerToUpdate = {"apt-get": " update", "pacman": " -Sy", "dnf": " update", "yum": " update", "emerge" : " --sync", "zypper":" refresh"}
+    completedProcess = subprocess.run([pkg_manager + managerToUpdate[pkg_manager]], shell=True)
+    if (completedProcess.returncode == 0):
+        print("[+] Synchronized with package repository")
+    else:
+        print("[-] Error while synchronizing with packages repository.")
+
+    common_pkg = ["gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "xfsprogs", "btrfs-progs", "pcmciautils", "ppp", "grub","iptables","openssl", "bc"]
+    print("[*] Installation...")
+    returnCode = installPackages(common_pkg)
+    # Now packages with non common name.
+    debian_specific = ["reiserfsprogs", "squashfs","quotatool", "nfs-kernel-server","procps", "mcelog", "libcrypto++6"]
+
+    arch_specific = ["reiserfsprogs", "squashfs-tools","quota-tools", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile"]
+
+    redHat_specific =  ["reiserfs-utils", "squashfs-tools", "quotatool","isdn4k-utils", "nfs-utils", "procps-ng", "oprofile", "mcelog"]
+
+    gentoo_specific =  ["reiserfsprogs", "squashfs-tools", "quotatool", "nfs-utils", "procps", "mcelog", "oprofile"]
+
+    suse_specific =  ["reiserfs", "quota", "nfs-client", "procps"]
+
+    packageSpecific = {"apt-get" : debian_specific, "pacman" : arch_specific, "dnf":redHat_specific, "yum":redHat_specific, "emerge":gentoo_specific, "zypper":suse_specific}
+    installPackages(packageSpecific[pkg_manager])
 
 installDependency()
