d978e8bce51818ce72724516300ad5a637041bd9
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2017-10-25 15:08:37 +0200

    Corrections mineures

--- installDependencies.py
+++ installDependencies.py
@@ -2,14 +2,14 @@
 import subprocess
 import shutil
 
-# author : LE FLEM Erwan, MERZOUK Fahim
+# authors : LE FLEM Erwan, MERZOUK Fahim
 # Check if the given package manager is present on the system
 # return true or false
 def packageManagerIs(packageManagerName):
     return shutil.which(packageManagerName)
 
-# author : LE FLEM Erwan, MERZOUK Fahim
-# get the package manager present on the system
+# authors : LE FLEM Erwan, MERZOUK Fahim
+# get the package manager presents on the system
 # return value :
 #    as a String, the name of the first supported package manager, if any.
 #    None if no supported packages manager has been found e.g pacman.
@@ -20,9 +20,9 @@ def getPackageManager():
             return manager
     return None
 
-# author : LE FLEM Erwan, MERZOUK Fahim
+# authors : LE FLEM Erwan, MERZOUK Fahim
 # Install the list of given packages
-# return 0 on sucess, -2 if no supported packages manager is found, and other code for other error.
+# return 0 on sucess, -2 if no supported packages manager is found, and other code if the package managers failed installation.
 def installPackages(listOfPackages):
     managerToCmdInstall = {"apt-get": " -qq -y install ", "pacman": " -S --noconfirm ", "dnf": " -q install ", "yum": " -y install ", "emerge" : " --quiet-build=y -a ", "zypper":" --non-interactive install "}
     pkg_manager = getPackageManager();
@@ -33,16 +33,16 @@ def installPackages(listOfPackages):
         print("[-] Unsupported package manager")
         return -2
 
-# author : LE FLEM Erwan, MERZOUK Fahim
-# Install packages of required dependencies to compile the kernel
-#return zero on sucess, =/= on error.
+#authors : LE FLEM Erwan, MERZOUK Fahim
+#Install packages of required dependencies to compile the kernel
+#return zero on sucess, - 2 if no packages manager is found on error, other values for installation error.
 def installDependency():
     pkg_manager = getPackageManager();
 
     if (pkg_manager == None):
         print("[-] Unsupported package manager")
         return -2
-    # TODO : check on https://wiki.archlinux.org/index.php/System_maintenance#Partial_upgrades_are_unsupported about issues using -Sy instead of -Syu before installing pkgs.
+    # TODO : check on https://wiki.archlinux.org/index.php/System_maintenance#Partial_upgrades_are_unsupported about potential issues using -Sy instead of -Syu before installing pkgs.
     managerToUpdate = {"apt-get": " update", "pacman": " -Sy", "dnf": " update", "yum": " update", "emerge" : " --sync", "zypper":" refresh"}
     completedProcess = subprocess.run([pkg_manager + managerToUpdate[pkg_manager]], shell=True)
     if (completedProcess.returncode == 0):
@@ -50,10 +50,15 @@ def installDependency():
     else:
         print("[-] Error while synchronizing with packages repository.")
 
+    #Installation of package with name common for all distributions.
     common_pkg = ["gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "xfsprogs", "btrfs-progs", "pcmciautils", "ppp", "grub","iptables","openssl", "bc"]
     print("[*] Installation...")
     returnCode = installPackages(common_pkg)
-    # Now packages with non common name.
+
+    if (returnCode != 0)
+        print("[-] Error while installing common packages")
+        return returnCode;
+    # Now installation of packages with name that vary amongs distributions
     debian_specific = ["reiserfsprogs", "squashfs","quotatool", "nfs-kernel-server","procps", "mcelog", "libcrypto++6"]
 
     arch_specific = ["reiserfsprogs", "squashfs-tools","quota-tools", "isdn4k-utils", "nfs-utils", "procps-ng", "oprofile"]
@@ -65,6 +70,10 @@ def installDependency():
     suse_specific =  ["reiserfs", "quota", "nfs-client", "procps"]
 
     packageSpecific = {"apt-get" : debian_specific, "pacman" : arch_specific, "dnf":redHat_specific, "yum":redHat_specific, "emerge":gentoo_specific, "zypper":suse_specific}
-    installPackages(packageSpecific[pkg_manager])
+    returnCode = installPackages(packageSpecific[pkg_manager])
+
+    if (returnCode != 0)
+        print("[-] Error while installing distrib specific packages")
+        return errorCode;
 
 installDependency()
