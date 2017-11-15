598ac291630be2b689dc4e59b02f720fbdcf01bf
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2017-11-15 17:48:18 +0100

    refactoring, mise en commun de code dupliqué avec tuxml.py

--- installDependencies.py
+++ installDependencies.py
@@ -12,7 +12,7 @@ def packageManagerIs(packageManagerName):
 # get the package manager presents on the system
 # return value :
 #    as a String, the name of the first supported package manager, if any.
-#    None if no supported packages manager has been found e.g pacman.
+#    None if no supported packages manager has been found e.g apt-get.
 def getPackageManager():
     pkg_managers = ["apt-get", "pacman", "dnf", "yum", "emerge", "zypper"]
     for manager in pkg_managers:
@@ -23,33 +23,30 @@ def getPackageManager():
 # authors : LE FLEM Erwan, MERZOUK Fahim
 # Install the list of given packages
 # return 0 on sucess, -2 if no supported packages manager is found, and other code if the package managers failed installation.
-def installPackages(listOfPackages):
+def installPackages(listOfPackages,pkg_manager):
     managerToCmdInstall = {"apt-get": " -qq -y install ", "pacman": " -S --noconfirm ", "dnf": " -q install ", "yum": " -y install ", "emerge" : " --quiet-build=y -a ", "zypper":" --non-interactive install "}
-    pkg_manager = getPackageManager();
-    if(pkg_manager != None):
-        completedProcess = subprocess.run([pkg_manager + managerToCmdInstall[pkg_manager] + " ".join(listOfPackages)], shell=True)
-        return completedProcess.returncode
-    else:
-        print("[-] Unsupported package manager")
-        return -2
+    return subprocess.call([pkg_manager + managerToCmdInstall[pkg_manager] + " ".join(listOfPackages)], shell=True)
+
+def update_system(pkg_manager):
+    # TODO : check on https://wiki.archlinux.org/index.php/System_maintenance#Partial_upgrades_are_unsupported about potential issues using -Sy instead of -Syu before installing pkgs.
+    managerToUpdate = {"apt-get": " update && apt-get upgrade", "pacman": " -Syu", "dnf": " update && dnf upgrade", "yum": " update", "emerge" : " --sync", "zypper":" refresh"}
+    return subprocess.call([pkg_manager + managerToUpdate[pkg_manager]], shell=True)
+
+    # if (code == 0):
+    #     print("[+] Synchronized with package repository")
+    # else:
+    #     print("[-] Error while synchronizing with packages repository.")
 
 #authors : LE FLEM Erwan, MERZOUK Fahim
 #Install packages of required dependencies to compile the kernel
 #return zero on sucess, - 2 if no packages manager is found on error, other values for installation error.
 def installDependency():
     pkg_manager = getPackageManager();
-
     if (pkg_manager == None):
         print("[-] Unsupported package manager")
         return -2
-    # TODO : check on https://wiki.archlinux.org/index.php/System_maintenance#Partial_upgrades_are_unsupported about potential issues using -Sy instead of -Syu before installing pkgs.
-    managerToUpdate = {"apt-get": " update", "pacman": " -Sy", "dnf": " update", "yum": " update", "emerge" : " --sync", "zypper":" refresh"}
-    completedProcess = subprocess.run([pkg_manager + managerToUpdate[pkg_manager]], shell=True)
-    if (completedProcess.returncode == 0):
-        print("[+] Synchronized with package repository")
-    else:
-        print("[-] Error while synchronizing with packages repository.")
 
+    update_package_manager()
     #Installation of package with name common for all distributions.
     common_pkg = ["gcc", "make", "binutils", "util-linux", "kmod", "e2fsprogs", "jfsutils", "xfsprogs", "btrfs-progs", "pcmciautils", "ppp", "grub","iptables","openssl", "bc"]
     print("[*] Installation...")
@@ -57,7 +54,8 @@ def installDependency():
 
     if (returnCode != 0):
         print("[-] Error while installing common packages")
-        return returnCode;
+        return returnCode
+
     # Now installation of packages with name that vary amongs distributions
     debian_specific = ["reiserfsprogs", "squashfs","quotatool", "nfs-kernel-server","procps", "mcelog", "libcrypto++6"
     ,"apt-utils"]
@@ -75,6 +73,4 @@ def installDependency():
 
     if (returnCode != 0):
         print("[-] Error while installing distrib specific packages")
-        return errorCode;
-
-installDependency()
+        return errorCode
