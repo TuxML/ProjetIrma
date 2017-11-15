ec0d0bc8f6fff2414132b79bae5b8ef4a42aeda3
CaptainMexiko <didotgwendal@gmail.com>
2017-11-15 15:56:30 +0100

    Test des retours d'erreur

--- TPDIM.py
+++ TPDIM.py
@@ -4,14 +4,19 @@ import os
 
 # Start of the script
 
-print("Welcome, this is the TuxML Project Docker image manager")
-print("What do you want to do ?")
+print("Welcome, this is the TuxML Project Docker Image Manager")
+# print("What do you want to do ?")
+
+# print("B : Build une nouvelle image \n P : Build et Push une image existante")
 
 print("Update of the docker image tuxml/tuxmldebian")
 
 # Build the choosen docker image
 str1 = 'sudo docker build -t tuxml/tuxmldebian .'
-os.system(str1)
+resb = os.system(str1)
+
+if resb != 0:
+    print("erreur {}".format(resb))
 
 print("Push of the image on the distant repository")
 
