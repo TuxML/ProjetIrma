f6485bcb1e1d8720a5ea30a724c0368763636856
alemasle <alexislm35@hotmail.fr>
2017-11-16 11:08:22 +0100

    modification temporaire tuxml.py
    Print en plus pour débug

--- tuxLogs.py
+++ tuxLogs.py
@@ -2,5 +2,6 @@
 
 import os
 
+# The command which we print and we write the output in the tuxML.logs
 chaine = '/TuxML/tuxml.py /TuxML/linux-4.13.3 --debug | tee /TuxML/linux-4.13.3/logs/tuxML.logs'
 os.system(chaine)
