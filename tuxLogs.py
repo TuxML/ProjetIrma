0c13d3b4bd7b1bb3d1c8fb8ef7c23e60d36de95c
alemasle <32134441+alemasle@users.noreply.github.com>
2017-11-26 15:11:20 +0100

    Supression du scotch apt update provisoire

--- tuxLogs.py
+++ tuxLogs.py
@@ -4,8 +4,5 @@ import os
 
 # The command which we print and we write the output in the tuxML.logs
 
-print(':Updating:')
-os.system('apt update')
-
 print('Starting tuxml.py ...')
 os.system('/TuxML/core/tuxml.py /TuxML/linux-4.13.3 -v')
