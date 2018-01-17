a0da69cf367a580de59155dd4cee16c161d26cd8
CaptainMexiko <didotgwendal@gmail.com>
2018-01-17 14:41:16 +0100

    Mise a jour de TPDIM, gestion d'option du script

--- TPDIM.py
+++ TPDIM.py
@@ -2,13 +2,17 @@
 
 import os
 from sys import argv
+import argparse
 
 # We define the function use in the script
+
+
 def dockerBuild():
     print("Update of the docker image tuxml/tuxmldebian")
     # Build the choosen docker image
     str1 = 'sudo docker build -t tuxml/tuxmldebian .'
     os.system(str1)
+
 
 def dockerPush():
     print("Push of the image on the distant repository")
@@ -22,20 +26,13 @@ def dockerPush():
 
 # Start of the script
 
-# Refactor needed with the use of argparse
-# We check if the script have an option to it, if not we run the program with GUI (not yet implement)
-if len(argv) == 1:
-    print("The graphical mode of TPDIM is not implemented yet.")
-    exit(0)
 
-# Show the options if the user ask for it
-if "-h" in argv or "--help" in argv:
-    print("")
-    print("Options : -b [.|directory path], --build     Build an image with the folder give in option, the folder must have a valid DockerFile")
-    print("          -g <parent image> [dependances text file], --generate <parent_image> [dependances text file]    Generate a DockerFile, suit for TuxML, with the parent image and add the optional dependances.")
-    exit(0)
+if __name__ == "__main__":
 
-# We check if the user is a super-user.
-if os.getuid() != 0:
-    print('Docker needs to be launch with super user rights')
-    exit(1)
+    parser = argparse.ArgumentParser(description="test", formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-b', '--build', help="Folder where is locate a valid docker file use to build a docker image, default is  [.] ", default=".")
+    parser.add_argument('-g', '--generate', help="Image use to generate a docker file")
+    parser.add_argument('-dep', '--dependences', help="Dependences you want to add to your docker image when you generate your dockerfile")
+
+args = parser.parse_args()
