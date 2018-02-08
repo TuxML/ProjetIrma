8a539e1c9c878dbe5b86c8bd87b2652d8d593395
alemasle <alexislm35@hotmail.fr>
2018-02-08 21:54:54 +0100

    Nouvelle méthode

--- gcc-learn/Fetch.py
+++ gcc-learn/Fetch.py
@@ -15,15 +15,9 @@ except Exception as e:
     print("You need to give an integer")
     exit(1)
 
-# Used to differenciate the Fetch run by users with Fetch called in a docker
-# It runs with a different behavior
-if "--comp" in argv:
-    os.system("../core/tuxml.py ../linux-4.13.3/ -d ConfigFile/" + argv[3] + ".config -c " + str(n))
-    exit(0)
-
 # Compile all the .config file
 for i in range(1):
     print("Docker num " + str(i))
-    chaine = 'sudo docker run -it tuxml/tuxmldebian:dev "cd TuxML; git pull; git checkout GCC-Learning; cd gcc-learn; chmod +x Fetch.py; ./Fetch.py ' + str(n) + ' --comp ' + str(i) + '"'
+    chaine = 'sudo docker run -it tuxml/tuxmldebian:dev /TuxML/gcc-learn/ExeConfig.py ' + str(n) + ' ' + str(i)
     os.system(chaine)
     print("")
