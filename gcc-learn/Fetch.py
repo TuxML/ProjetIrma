26ef3eed9f9f489a805a96d4bdea18af5c0be608
alemasle <alexislm35@hotmail.fr>
2018-02-14 17:56:27 +0100

    mauvaise typo

--- gcc-learn/Fetch.py
+++ gcc-learn/Fetch.py
@@ -21,6 +21,6 @@ os.system("sudo docker pull tuxml/tuxmldebian:dev")
 # Compile all the .config file
 for i in range(1):
     print("Docker num " + str(i))
-    chaine = 'sudo docker run -it tuxml/tuxmldebian:dev /TuxML/gcc-learn/ExeConfig.py ' + str(n) + ' ' + str(i)
+    chaine = 'sudo docker run -it tuxml/tuxmldebian:dev /TuxML/gcc-learn/ExecConfig.py ' + str(n) + ' ' + str(i)
     os.system(chaine)
     print("")
