8502112c98fc5cb4655d6c05aa20b0d67a9bf9c8
CaptainMexiko <didotgwendal@gmail.com>
2018-02-28 14:46:28 +0100

    Mise a jour de TPDIM (a tester changement d'OS)

--- TPDIM.py
+++ TPDIM.py
@@ -6,22 +6,22 @@ import argparse
 # We define the function use in the script
 
 
-def DockerBuild(image, *location):
+def DockerBuild(image, tag, *location):
     print("Update of the docker image")
     # Build the choosen docker image
     if location in args:
-        strBuild = 'sudo docker build -t {} {}'.format(image, location)
+        strBuild = 'sudo docker build -t tuxml/tuxml{}:{} {}'.format(image, tag, location)
         print(strBuild)
         pass
     else:
-        strBuild = 'sudo docker build -t tuxml/tuxml{} .'.format(image)
+        strBuild = 'sudo docker build -t tuxml/tuxml{}:{} .'.format(image, tag)
     os.system(strBuild)
 
 
-def DockerPush(repository):
+def DockerPush(repository, tag):
     print("Push of the image on the distant repository")
     # Push of the docker image on docker hub
-    strpush = 'sudo docker push tuxml/tuxml{}'.format(repository)
+    strpush = 'sudo docker push tuxml/tuxml{}:{}'.format(repository, tag)
     rstrpush = os.system(strpush)
     # If needed, login to the repository
     if rstrpush == 256:
@@ -30,24 +30,24 @@ def DockerPush(repository):
         os.system(str3)
 
 
-def DockerGenerate(originImage, *dependencesFile):
-    newImage = 'tuxml/{}tuxml:prod'.format(originImage)
+def DockerGenerate(originImage, tag, *dependencesFile):
+    newImage = 'tuxml/{}tuxml:{}'.format(originImage, tag)
     os.chdir('BuildImageInter')
     dockerFileI = open("Dockerfile", "w")
-    dockerFileI.write("FROM {}:latest\n".format(originImage))
+    dockerFileI.write("FROM {}:{}\n".format(originImage, tag))
     depText = ""
     if len(dependencesFile) != 0:
         depText = dependencesFile[0]
     dockerFileI.write("ADD linux-4.13.3 /TuxML/linux-4.13.3\nRUN apt-get update && apt-get -qq -y install " + depText + " \nRUN wget https://bootstrap.pypa.io/get-pip.py\nRUN python3 get-pip.py\nRUN pip3 install mysqlclient\nRUN apt-get clean && rm -rf /var/lib/apt/lists/*\nEXPOSE 80\nENV NAME World")
     dockerFileI.close()
-    strBuildI = 'sudo docker build -t tuxml/{}tuxml:prod .'.format(originImage)
+    strBuildI = 'sudo docker build -t tuxml/{}tuxml:{} .'.format(originImage, tag)
     os.system(strBuildI)
-    strPushI = 'sudo docker push tuxml/{}tuxml:prod'.format(originImage)
+    strPushI = 'sudo docker push tuxml/{}tuxml:{}'.format(originImage, tag)
     os.system(strPushI)
     os.chdir('..')
     dockerFile = open("Dockerfile", "w")
     dockerFile.write("FROM {}\n".format(newImage))
-    dockerFile.write("ADD core /TuxML\nADD gcc-learn/ExecConfig.py /TuxML/gcc-learns \nADD tuxLogs.py /TuxML\nEXPOSE 80\nENV NAME World\nLABEL Description \"Image TuxML\"\n")
+    dockerFile.write("ADD core /TuxML\nADD gcc-learn/ExecConfig.py /TuxML/gcc-learn/ExecConfig.py \nADD gcc-learn/ConfigFile /TuxML/gcc-learn/ \nADD tuxLogs.py /TuxML\nEXPOSE 80\nENV NAME World\nLABEL Description \"Image TuxML\"\n")
     dockerFile.close()
 
 
@@ -64,6 +64,7 @@ if __name__ == "__main__":
     parser.add_argument('-g', '--generate', help="Image use to generate a docker file")
     parser.add_argument('-dep', '--dependences', help="Dependences you want to add to your docker image when you generate your dockerfile")
     parser.add_argument('-p', '--push', help="Push the image on the distant repository")
+    parser.add_argument('-t', '--tag', help="Tag of the image you want to generate/build/push")
 
 args = parser.parse_args()
 
@@ -78,22 +79,22 @@ if args.generate:
                 depText = args.dependences
                 openDep = open(depText)
                 strDep = openDep.read()
-                DockerGenerate(args.generate, strDep)
+                DockerGenerate(args.generate, args.tag, strDep)
             else:
-                DockerGenerate(args.generate)
+                DockerGenerate(args.generate, args.tag)
         else:
             print("Canceled")
             exit(0)
 
 if args.push:
-    DockerPush(args.push)
+    DockerPush(args.push, args.tag)
 if args.build:
     if args.folder:
         DocPre = os.listdir(args.folder)
         if "Dockerfile" not in DocPre:
-            print("Please give a folder with a valid Dockerfile")
+            print("Please give a folder with a vdevalid Dockerfile")
             exit(-1)
         else:
-            DockerBuild(args.build, args.folder)
+            DockerBuild(args.build, args.tag, args.folder)
     else:
-        DockerBuild(args.image)
+        DockerBuild(args.image, args.tag)
