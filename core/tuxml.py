6f1af855b58c38ac46c5b14e7023fb5b60fcee98
micleb <lebreton.mickael@protonmail.com>
2018-02-28 15:24:22 +0100

    intégration de bootchecker au fichier tuxml.py

--- core/tuxml.py
+++ core/tuxml.py
@@ -256,7 +256,15 @@ def main():
         status = end_time - start_time
         compile_time = time.strftime("%H:%M:%S", time.gmtime(status))
         tcom.pprint(0, "Successfully compiled in {}".format(compile_time))
-        # TODO tests
+
+        # launching tests
+        start_time = time.time()
+        status = bootTry(tset.PATH)
+        end_time = time.time()
+        if status == 0:
+            boot_time = end_time - start_time
+        else:
+            boot_time = status
     else:
         tcom.pprint(1, "Unable to compile using this KCONFIG_FILE, status={}".format(status))
 
--- core/tuxml_bootCheck.py
+++ core/tuxml_bootCheck.py
@@ -4,52 +4,51 @@
 import subprocess
 import time
 import re
+import tuxml_common as tcom
 
 # Function used to boot up a given kernel
 # WARNING : USES qemu-system-x86_64 !
 # path is the path to the sources of the kernel
-# Returns 0 if the kernel did boot, 1 if it did not and 99 if the check timed out
-# Return -2 if the subprocess call failed for whatever reason
+# Returns 0 if the kernel did boot, -1 if it did not and -2 if the check timed out
+# Return -3 if the subprocess call failed for whatever reason
 def bootTry(path):
+	tcom.pprint(2, "Launching boot test on kernel")
 
 	cmd = "qemu-system-x86_64"
 	sbStatus = subprocess.call(["mkinitramfs","-o", path + "/arch/x86_64/boot/initrd.img-4.13.3"])
-	
+
 	if sbStatus == 0:
-		
+
 		procId = subprocess.Popen([cmd, "-kernel", path + "/arch/x86_64/boot/bzImage", "-initrd", path + "/arch/x86_64/boot/initrd.img-4.13.3", "-m", "1G", "-append", "console=ttyS0,38400", "-serial", "file:serial.out"])
-	
+
 		rndCounter = 1
-		status = -1
-	
+		status = 1
+
 		time.sleep(5)
 		outFile = open("serial.out",mode='r')
-		
-		while status == -1:
+
+		while status == 1:
 			time.sleep(10)
-			print("Reading output file for boot end, attempt", rndCounter)
+			tcom.pprint(3, "Reading output file for boot end, attempt", rndCounter)
 			fileData = outFile.read()
-		
-			if re.search("end Kernel panic", fileData):
-				print("Kernel panic detected, kernel probably failed to boot !")
-				status = 1
-			
+
 			if re.search("(initramfs)", fileData):
-				print("Shell detected, kernel successfully booted !")
+				tcom.pprint(0, "Shell detected, kernel successfully booted")
 				status = 0
-				
+
+			if re.search("end Kernel panic", fileData):
+				tcom.pprint(1, "Kernel panic detected, kernel probably failed to boot")
+				status = -1
+
 			if rndCounter == 61:
-				print("More than 60 attempts at reading, possible infinite loop in boot process, interrupting")
-				status = 99
-				
+				tcom.pprint(1, "More than 60 attempts at reading, possible infinite loop in boot process, interrupting")
+				status = -2
+
 			rndCounter = rndCounter + 1
-				
+
 		procId.terminate() # <-- terminate the process
 		outFile.close()
 		return status
-		
-	else:
-		return -2
 
-# Testing main program, TODO : Remove once tuxml_bootCheck is integrated into tuxml.py
-bootTry("/root/Downloads/linux-4.13.3/")
+	else:
+		return -3
