5c6779a18b49467c8caa50fc0be0d88cf95ae784
alemasle <32134441+alemasle@users.noreply.github.com>
2018-03-15 22:30:49 +0100

    Suppression d'import de module inutile (colors)

--- MLfood.py
+++ MLfood.py
@@ -2,15 +2,35 @@
 
 import os
 import time
-import colors as color
 
 from sys import argv
 
 # Author Alexis LE MASLE
 
+
+## COLORS
+WHITE           = "\033[0m"                # Default color
+GRAY            = "\033[38;5;7m"           # Debug
+BLACK           = "\033[38;5;16m"
+RED             = "\033[38;5;1m"           # Errors messages
+LIGHT_RED       = "\033[38;5;9m"
+GREEN           = "\033[38;5;2m"           # Success messages
+LIGHT_GREEN     = "\033[38;5;10m"
+ORANGE          = "\033[38;5;3m"           #
+LIGHT_ORANGE    = "\033[38;5;11m"
+BLUE_1          = "\033[38;5;4m"
+LIGHT_BLUE_1    = "\033[38;5;12m"
+BLUE_2          = "\033[38;5;6m"
+LIGHT_BLUE_2    = "\033[38;5;14m"
+PURPLE          = "\033[38;5;5m"
+LIGHT_PURPLE    = "\033[38;5;13m"
+
+
+
+
 # Error if there is no argument "number" of compilation to run.
 if len(argv) == 1 or "-h" in argv or "--help" in argv:
-    print(color.LIGHT_BLUE_1 + "")
+    print(LIGHT_BLUE_1 + "")
     print("Try: ./MLfood.py <Integer> [<Integer>] [Options]")
     print("")
     print("The first Integer run MLfood into the given number of containers")
@@ -21,29 +41,29 @@ if len(argv) == 1 or "-h" in argv or "--help" in argv:
     print("         -h, --help    Prompt Options")
     print("         --reset-logs  Delete all the saved logs")
     print("         --dev         Use images in current developpement")
-    print("" + color.GRAY)
+    print("" + GRAY)
     exit(0)
 
 # We check if the user is a super-user.
 # Restarting with sudo.
 if os.getuid() != 0:
-    print(color.LIGHT_BLUE_1 + 'Docker needs to start with sudo mode' + color.GRAY)
+    print(LIGHT_BLUE_1 + 'Docker needs to start with sudo mode' + GRAY)
 
 # Check if there is the --reset-logs option to erase all the logs.
 if "--reset-logs" in argv:
-    print(color.ORANGE + "Are-you sure you want to delete all the saved logs? (y/n)")
+    print(ORANGE + "Are-you sure you want to delete all the saved logs? (y/n)")
     reset = input()
     reset.lower()
     if reset == "y":
         print("Deleting all the logs in Logs/...")
         os.system("rm -rf Logs/*")
         print("Delete done.")
-        print("" + color.GRAY)
+        print("" + GRAY)
         exit(0)
     else:
         print("")
         print("Logs are not deleted.")
-        print("" + color.GRAY)
+        print("" + GRAY)
         exit(0)
 
 # Convert the parameter in an Integer which is the number of compilation to do.
@@ -51,7 +71,7 @@ if "--reset-logs" in argv:
 try:
     nb = int(argv[1])
     if nb >= 50:
-        print(color.ORANGE + "Are-you sure you want to start " + str(nb) + " compilation? (y/n)")
+        print(ORANGE + "Are-you sure you want to start " + str(nb) + " compilation? (y/n)")
         print('Canceling it would take as much Ctrl+C as the remaining number of compiling.')
         ok = input()
         ok.lower()
@@ -63,11 +83,11 @@ except Exception as e:
     print("Command ./MLfood.py <Integer> [Option]")
     exit(0)
 
-print(color.GRAY)
+print(GRAY)
 
 # Retrieves the number of compilation to run.
 if nb <= 0:
-    print(color.RED + "Please enter a non-zero positive integer." + color.GRAY)
+    print(RED + "Please enter a non-zero positive integer." + GRAY)
     exit(0)
 
 # Must contain the list of differents systems images URLs with the execution tuxml script.
@@ -106,9 +126,9 @@ for i in range(nb):
 
     # Main command which run a docker which execute the tuxLogs.py script and write the logs in output.logs
     chaine = 'sudo docker run -i ' + images[i % len(images)] + ' /TuxML/tuxLogs.py ' + str(incrN) + " " + dev + ' | tee Logs/' + logsFolder + '/output.log'
-    print(color.LIGHT_BLUE_1 + "\n=============== Docker number " + str(i + 1)+ " ===============")
+    print(LIGHT_BLUE_1 + "\n=============== Docker number " + str(i + 1)+ " ===============")
     print(chaine)
-    print("==========================================\n" + color.GRAY)
+    print("==========================================\n" + GRAY)
     os.system(chaine)
 
     # Get the logs std.logs and err.logs from the last used container and retrieves the ".config" file.
@@ -118,23 +138,23 @@ for i in range(nb):
     stdlogs = 'sudo docker cp ' + dock + ':/TuxML/linux-4.13.3/logs/std.log ./Logs/' + logsFolder
     errlogs = 'sudo docker cp ' + dock + ':/TuxML/linux-4.13.3/logs/err.log ./Logs/' + logsFolder
     configFile = 'sudo docker cp ' + dock + ':/TuxML/linux-4.13.3/.config ./Logs/' + logsFolder + '/' + logsFolder + '.config'
-    print(color.LIGHT_BLUE_1 + "Fetch logs and .config file to the folder ./Logs/" + logsFolder + " " + color.GRAY)
+    print(LIGHT_BLUE_1 + "Fetch logs and .config file to the folder ./Logs/" + logsFolder + " " + GRAY)
     os.system(stdlogs)
     os.system(errlogs)
     os.system(configFile)
 
     # Clean all the containers used previously.
     if "--no-clean" not in argv:
-        print(color.LIGHT_PURPLE + "Cleaning containers . . .")
+        print(LIGHT_PURPLE + "Cleaning containers . . .")
         os.system("sudo docker rm -v $(sudo docker ps -aq)")
         print("Clean done!")
         print("")
     elif argv[2] != null:
-        print(color.RED + "Option " + argv[2] + " unknown." + color.GRAY)
+        print(RED + "Option " + argv[2] + " unknown." + GRAY)
         print("")
         exit(0)
 
-    print("" + color.GRAY)
+    print("" + GRAY)
 
 # The end
 print(color.LIGHT_BLUE_1 + "Your tamago... database ate " + str(nb) + " compilation data, come back later to feed it!" + color.GRAY)
--- gcc-learn/ExecConfig.py
+++ gcc-learn/ExecConfig.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 
 import os
-import colors as color
 from sys import argv
 
 # Author Alexis LE MASLE
 
+## COLORS
+WHITE           = "\033[0m"                # Default color
+GRAY            = "\033[38;5;7m"           # Debug
+BLACK           = "\033[38;5;16m"
+RED             = "\033[38;5;1m"           # Errors messages
+LIGHT_RED       = "\033[38;5;9m"
+GREEN           = "\033[38;5;2m"           # Success messages
+LIGHT_GREEN     = "\033[38;5;10m"
+ORANGE          = "\033[38;5;3m"           #
+LIGHT_ORANGE    = "\033[38;5;11m"
+BLUE_1          = "\033[38;5;4m"
+LIGHT_BLUE_1    = "\033[38;5;12m"
+BLUE_2          = "\033[38;5;6m"
+LIGHT_BLUE_2    = "\033[38;5;14m"
+PURPLE          = "\033[38;5;5m"
+LIGHT_PURPLE    = "\033[38;5;13m"
+
 if len(argv) < 3:
-	print(color.LIGHT_BLUE_1 + "Use:")
-	print("./ExecConfig.py [num .config] [nb core]" + color.GRAY)
+	print(LIGHT_BLUE_1 + "Use:")
+	print("./ExecConfig.py [num .config] [nb core]" + GRAY)
 	exit(0)
 
 if int(argv[2]) <= 0:
-	print(color.RED + "Please enter a non zero positive number of core." + color.GRAY)
+	print(RED + "Please enter a non zero positive number of core." + GRAY)
 	exit(0)
 
 # It runs with a different behavior
