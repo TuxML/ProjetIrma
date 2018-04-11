c22e7cbfa1d02acfb72b1199f194e51baab9954c
alemasle <32134441+alemasle@users.noreply.github.com>
2018-04-11 18:23:09 +0200

    premier test du fichier pour output.log

---
+++ runandlog.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import argparse
+from sys import argv
+
+# Author Alexis LE MASLE
+
+# Creation of help and argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("incremental", help = "The incremental factor (0 by default)", type=int, nargs='?', default=0)
+args = parser.parse_args()
+
+# Run tuxml.py and retrieves the output converted in a log file.
+print("")
+print('Running tuxLogs.py ...')
+print("")
+chaine = '/TuxML/tuxLogs.py ' + str(args.incremental) + ' | tee output.log'
+print("")
+os.system(chaine)
