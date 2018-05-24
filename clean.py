7ae02aa2694c81f311a003c133c65549332ba454
alemasle <alexislm35@outlook.fr>
2018-05-24 15:02:48 +0200

    Clean script improved

--- clean.py
+++ clean.py
@@ -4,14 +4,26 @@ import subprocess
 import argparse
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Clean the Logs/ directory, and all docker containers (running or not) if no arguments are precised")
+parser.add_argument("--logs", help='Only clean "Logs/"', action="store_true")
+parser.add_argument("--docker", help='Only clean docker containers', action="store_true")
 args = parser.parse_args()
 
-print("Deleting Logs...")
-subprocess.run("sudo rm -rf Logs/*", shell=True)
-print("")
-print("Stopping running Docker containers:")
-subprocess.run("sudo docker stop $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
-print("")
-print("Deleting Docker containers:")
-subprocess.run("sudo docker rm $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
+if args.logs:
+        print("Deleting Logs...")
+        subprocess.run("sudo rm -rf Logs/*", shell=True)
+elif args.docker:
+        print("Stopping running Docker containers:")
+        subprocess.run("sudo docker stop $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
+        print("")
+        print("Deleting Docker containers:")
+        subprocess.run("sudo docker rm $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
+else:
+    print("Deleting Logs...")
+    subprocess.run("sudo rm -rf Logs/*", shell=True)
+    print("")
+    print("Stopping running Docker containers:")
+    subprocess.run("sudo docker stop $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
+    print("")
+    print("Deleting Docker containers:")
+    subprocess.run("sudo docker rm $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
