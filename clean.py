f569d69ff5889281f4f0ebf812aeabd01da9b686
alemasle <alexislm35@outlook.fr>
2018-05-24 11:40:59 +0200

    Permet de nettoyer les logs et containers dockers

---
+++ clean.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import subprocess
+import argparse
+
+
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+print("Deleting Logs:")
+subprocess.run("sudo rm -rf Logs/*", shell=True)
+print("")
+print("Stopping running Docker containers:")
+subprocess.run("sudo docker stop $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
+print("")
+print("Deleting Docker containers:")
+subprocess.run("sudo docker rm $(sudo docker ps -aq)", shell=True, stderr=subprocess.DEVNULL)
