093f8d451cb32c89ad5cd79e2f01ac7b93034182
alemasle <alexislm35@outlook.fr>
2018-05-29 13:35:33 +0200

    test de ssh script

--- compilIstic.sh
+++ compilIstic.sh
@@ -7,10 +7,12 @@ machine=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10")
 echo -n Password:
 read -s password
 
-for elem in ${list[@]}
-do
-  for m in ${machine[@]}
-  do
-    sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
-  done
-done
+# for elem in ${list[@]}
+# do
+#   for m in ${machine[@]}
+#   do
+#     sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
+#   done
+# done
+
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt alemasle@131.254.18.201 "echo test& echo suite& echo fin"
