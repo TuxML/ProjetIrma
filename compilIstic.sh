b177cb069769b5a06d11154f41468dc7c5fed64d
alemasle <alexislm35@outlook.fr>
2018-05-30 09:40:20 +0200

    retour tests

--- compilIstic.sh
+++ compilIstic.sh
@@ -1,4 +1,4 @@
-list=("e008m" "e005m" "e010m" "e103m" "e105m" "e212m")
+list=("e008m" "e010m" "e103m" "e105m" "e212m")
 
 machine=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10")
 
@@ -6,15 +6,15 @@ echo -n Password:
 read -s password
 echo
 
-for elem in ${list[@]}
-do
-  for m in ${machine[@]}
-  do
-    #sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
-    ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "sudo ~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
-  done
-done
-# for x in $(seq 0 3)
+# for elem in ${list[@]}
 # do
-#   ssh -o StrictHostKeyChecking=no -tt alemasle@131.254.18.201 "sudo ~/ProjetIrma/MLfood.py 1 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
+#   for m in ${machine[@]}
+#   do
+#     #sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
+#     ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "sudo ~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
+#   done
 # done
+for x in $(seq 0 3)
+do
+  ssh -o StrictHostKeyChecking=no -tt alemasle@131.254.18.201 "sudo ~/ProjetIrma/MLfood.py 1 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
+done
