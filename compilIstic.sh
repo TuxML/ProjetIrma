f7aa3932855a23cda55e1d8c75bddc54f69cf6b6
alemasle <alexislm35@outlook.fr>
2018-05-30 11:24:23 +0200

    Ajout compteur

--- compilIstic.sh
+++ compilIstic.sh
@@ -2,14 +2,19 @@ list=("e008m" "e010m" "e103m" "e105m" "e212m")
 
 machine=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10")
 
+cpt = 0
+
 # Machines de l'istic
 for elem in ${list[@]}
 do
   for m in ${machine[@]}
   do
+    cpt = $((cpt + 1))
     (echo \"$elem$m -- BEGIN\"; ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "echo \"$elem$m -- BEGIN\";~/TP/ProjetIrma/MLfood.py 1 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log --silent; exit" > /dev/null;  echo \"$elem$m -- DONE\")&
   done
 done
+
+echo "$cpt machine are used"
 
 # Works for sudo asking computer
 # for x in $(seq 0 3)
