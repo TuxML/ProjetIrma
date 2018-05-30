3167d2e8d2a2fdacf0dc79dc9018bd5b73019fe6
alemasle <alexislm35@outlook.fr>
2018-05-30 11:20:43 +0200

    Plus lisible/visible

--- compilIstic.sh
+++ compilIstic.sh
@@ -2,17 +2,12 @@ list=("e008m" "e010m" "e103m" "e105m" "e212m")
 
 machine=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10")
 
-# echo -n Password:
-# read -s password
-# echo
-
 # Machines de l'istic
 for elem in ${list[@]}
 do
   for m in ${machine[@]}
   do
-    #sshpass -p "$password" ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "~/TP/ProjetIrma/MLfood.py 100 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log"&
-    (ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "echo \"$elem$m -- BEGIN\";~/TP/ProjetIrma/MLfood.py 1 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log --silent > /dev/null; echo \"$elem$m -- DONE\"; exit")&
+    (echo \"$elem$m -- BEGIN\"; ssh -o StrictHostKeyChecking=no -tt 14008349@$elem$m.istic.univ-rennes1.fr "echo \"$elem$m -- BEGIN\";~/TP/ProjetIrma/MLfood.py 1 --force-compilation-limits --dev --no-kernel --no-logs --no-check-log --silent; exit" > /dev/null;  echo \"$elem$m -- DONE\")&
   done
 done
 
