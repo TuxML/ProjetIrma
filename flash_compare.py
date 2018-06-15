7337d51ec660dc9b2686844a778608c3da109990
alemasle <alexislm35@outlook.fr>
2018-06-15 14:22:41 +0200

    Comparing size into changes percentage and average

--- flash_compare.py
+++ flash_compare.py
@@ -9,28 +9,45 @@ parser = argparse.ArgumentParser()
 parser.add_argument("number", type=int, help="The number of comparisons to do (all by default)", nargs='?', default=-1)
 args = parser.parse_args()
 
+if not os.path.exists("./compare/"):
+    os.makedirs("./compare/")
+
+max_number = len([name for name in os.listdir('./compare/')])
+
 def diff_size(n):
 
     number = n
 
-    if not os.path.exists("./compare/"):
-        os.makedirs("./compare/")
-
-    max_number = len([name for name in os.listdir('./compare/')])
-
     if number > max_number:
         number = max_number
     elif number < 0:
-        number = 0
+        number = max_number
 
     print("Number of comparisons:", number, flush=True)
 
+    diff = {}
+
     for i in range(number):
 
-        print("Number:",i , flush=True)
-        subprocess.run('./bloat-o-meter compare/' + str(i) + '/incr-vmlinux compare/' + str(i) + '/basic-vmlinux > compare/' + str(i) + '/diff_size.txt', shell=True)
+        print("Number:",i , flush=True, end='')
+        subprocess.run('./bloat-o-meter compare/' + str(i) + '/incr-vmlinux compare/' + str(i) + '/basic-vmlinux > compare/' + str(i) + '/diff_size.txt', shell=True, stdout=open("compare/" + str(i) + "/diff_size.txt",'w'))
 
-    print("Size compared", flush=True)
+        with open("compare/" + str(i) + "/diff_size.txt", "r") as f:
+            lines = f.readlines()
+            tmp = lines[-1].split()[-1]
+            diff[str(i)] = tmp
+            print(" " + tmp, flush=True)
+
+    print("Percentage of differences between incremental and basic compiled kernels:")
+    liste = list(diff.values())
+    print(liste)
+
+    temp = 0.0
+    for i in range(len(liste)):
+        temp += float(liste[i][1:-1])
+
+    average = '+' + str(temp/len(liste))[:4] + '%'
+    print("average difference:", average, flush=True)
 
 if __name__ == "__main__":
     diff_size(args.number)
