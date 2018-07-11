86fc1a92d8fc8621d1e3438443c603e6843acb7c
alemasle <alexislm35@outlook.fr>
2018-07-11 16:29:31 +0200

    WIP
    Former-commit-id: effe4427456668e8acc1719865e730a83d3d6289

--- close_config.py
+++ close_config.py
@@ -7,22 +7,43 @@ import kconfiglib
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-y", "--yes", help="Number of options you want to set on 'y' (yes)")
-    parser.add_argument("-n", "--no", help="Number of options you want to set on 'n' (no)")
-    parser.add_argument("-m", "--modules", help="Number of options you want to set on 'm' (module)")
-    parser.add_argument("--path", help="Only edit an existing .config (create a new one by default)", nargs='?', default='.')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "-y", "--yes", help="Number of options you want to set on 'y' (yes)")
+    # parser.add_argument(
+    #     "-n", "--no", help="Number of options you want to set on 'n' (no)")
+    # parser.add_argument(
+    #     "-m", "--modules", help="Number of options you want to set on 'm' (module)")
+    # parser.add_argument(
+    #     "--path", help="Only edit an existing .config (create a new one by default)", nargs='?', default='.config')
+    # args = parser.parse_args()
 
     if len(argv) == 1:
-        parser.print_help()
+        # parser.print_help()
         exit(0)
 
-    print(" | ".join([k + ' : ' + str(vars(args)[k]) for k in vars(args)]), flush=True)
+    # print(" | ".join([k + ' : ' + str(vars(args)[k])
+    #                   for k in vars(args)]), flush=True)
 
-    config_file = args.path
+    # config_file = args.path
 
-    k = kconfiglib.Kconfig(filename=config_file)
+    k = kconfiglib.Kconfig()
+
+    # k.load_config(args.path)
+    k.load_config(".config")
+    # data_fill = []
+    tmp = k.defined_syms
+    
+    print("")
+    for i in range(10):
+        sym = tmp[i]
+        print(sym.name, ":", kconfiglib.TYPE_TO_STR[sym.orig_type].upper(), flush=True)
+
+    print("")
+    
+
+    # print(data_fill)
+
 
 if __name__ == "__main__":
     main()
