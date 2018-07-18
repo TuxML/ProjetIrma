50d12b75ddfda69787d0a8e1f9d79d498527d9b9
alemasle <alexislm35@outlook.fr>
2018-07-18 15:03:02 +0200

    Ergonomie augmentée
    Former-commit-id: 5692d5f9ff1dfbd8ae97dd5c388318ec4ca36dac

--- master_spring.py
+++ master_spring.py
@@ -12,23 +12,23 @@ def run():
 
 def setting(args):
     with open("params.txt", "w") as file:
-        for i in range(args.Number_of_machines):
-            file.write(str(args.Number_of_compilations) + '\n')
+        for i in range(args.nm):
+            file.write(str(args.nc) + '\n')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("Number_of_compilations", type=int,
-                        help="Number of compilations to do", nargs='?', default=1)
-    parser.add_argument("Number_of_machines", type=int,
-                        help="Number of Machines to use", nargs='?', default=1)
+    parser.add_argument("--nc", "--number_of_compilations", type=int,
+                        help="Number of compilations to do", default=1)
+    parser.add_argument("--nm", "--number_of_machines", type=int,
+                        help="Number of Machines to use", default=1)
     args = parser.parse_args()
 
     print(" | ".join([k + ' : ' + str(vars(args)[k]) for k in vars(args)]))
 
     setting(args)
 
-    run()
+    # run()
 
 
 if __name__ == '__main__':
