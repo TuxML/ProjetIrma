54da4d420ef22faf6996ff9a1ef5c22af480783f
PICARD Michaël <m.picard287@laposte.net>
2018-09-26 17:28:09 +0200

    WIP,DOC: updating build_image_tuxml.py
    Signed-off-by: PICARD Michaël <m.picard287@laposte.net>
    
    Former-commit-id: 370c7f529cffc30c2b4c7f14ed0390326a0b5d88

--- build_image_tuxml.py
+++ build_image_tuxml.py
@@ -8,8 +8,16 @@
 #  other than what a Docker image is (check https://docs.docker.com/get-started/ for more information).
 
 import subprocess
+import os
 
-
+## docker_build
+# @author DIDOT Gwendal, PICARD Michaël
+# @version 2
+# @brief build a docker image
+# @param image The image name of your choice. Default to None.
+# @param tag The tag of your choice. Default to None.
+# @param path The path where the Dockerfile is. Default to None, which is
+# equivalent to . (current directory).
 def docker_build(image=None, tag=None, path=None):
     if path is None:
         path = "."
@@ -23,9 +31,20 @@ def docker_build(image=None, tag=None, path=None):
     subprocess.call(args=strBuild)
 
 
+## create_dockerfile
+# @author DIDOT Gwendal, PICARD Michaël
+# @version 2
+# @brief Create and save a Dockerfile
+# @param content What you will actually put in your Dockerfile. Default to None.
+# @param path Where you want to save your Dockerfile. Default to None, which is
+# # equivalent to . (current directory).
 def create_dockerfile(content=None, path=None):
-    pass
+    if path is not None:
+        os.chdir(path)
+
+    with open("Dockerfile", "w") as file:
+        file.write(content)
 
 
-def copy_file(content=None, path=None):
-    pass
+
+
