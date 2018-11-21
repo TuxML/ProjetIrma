4de31e7232da792d4de13043020d1b7b67f7de4e
root <root@debian8.istic.univ-rennes1.fr>
2018-11-21 14:48:20 +0100

    add auto Y when apt install
    Former-commit-id: 2ad5f6b4ca7bf3265a6ef3a6944f118e23605a79

--- scriptBootKernel.sh
+++ scriptBootKernel.sh
@@ -2,9 +2,7 @@
  
 # Install required build tools on host machine ubuntu
 apt update
-apt install ncurses-dev build-essential libssl-dev libelf-dev
-apt install git
-apt install qemu
+apt install -y ncurses-dev build-essential libssl-dev libelf-dev git qemu
 
 # Prepare a working space for kernel development
 cd $HOME
@@ -35,24 +33,27 @@ cd $TOP/build/initramfs/busybox-x86
 mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
 cp -av $TOP/build/busybox-x86/_install/* .
 
-# cp $HOME/../init $TOP/build/initramfs/busybox-x86
+cp $HOME/../init $TOP/build/initramfs/busybox-x86
 chmod +x init
 # Generate initramfs
 find . -print0 \
    | cpio --null -ov --format=newc \
    | gzip -9 > $TOP/build/initramfs-busybox-x86.cpio.gz
 
-# Configure Linux using simple defconfig and kvm options
-#cd $TOP/linux
-#make O=$TOP/build/linux-x86-basic x86_64_defconfig
-#make O=$TOP/build/linux-x86-basic kvmconfig
+#Configure Linux using simple defconfig and kvm options
+cd $TOP/linux
+make O=$TOP/build/linux-x86-basic x86_64_defconfig
+make O=$TOP/build/linux-x86-basic kvmconfig
 
-# Build Linux
-#make O=$TOP/build/linux-x86-basic -j2
+#Build Linux
+make O=$TOP/build/linux-x86-basic -j2
 
-# Boot with Qemu
-#cd $TOP
-#qemu-system-x86_64 \
-#  -kernel build/linux-x86-basic/arch/x86_64/boot/bzImage \
-#  -initrd build/initramfs-busybox-x86.cpio.gz \
-#  -nographic -append "console=ttyS0"
+#Boot with Qemu
+cd $TOP
+qemu-system-x86_64 \
+  -kernel build/linux-x86-basic/arch/x86_64/boot/bzImage \
+  -initrd build/initramfs-busybox-x86.cpio.gz \
+  -nographic -append "console=ttyS0" >> log.txt &
+
+# Watch if Boot works or not
+./watchBootKernel.sh
