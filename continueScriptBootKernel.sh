525f2090f8feeec9b6f0f34226adecb9e21840fa
root <root@debian8.istic.univ-rennes1.fr>
2018-12-05 14:46:08 +0100

    Change >> by > on boot scripts
    Former-commit-id: 4595ec7dcbe014b13cf4f0b00fc3e335fd909ba6

--- continueScriptBootKernel.sh
+++ continueScriptBootKernel.sh
@@ -4,5 +4,5 @@ cd $HOME/kdev
 qemu-system-x86_64 \
   -kernel build/linux-x86-basic/arch/x86_64/boot/bzImage \
   -initrd build/initramfs-busybox-x86.cpio.gz \
-  -nographic -append "console=ttyS0" >> log.txt &
+  -nographic -append "console=ttyS0" > log.txt &
 
