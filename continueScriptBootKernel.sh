b01517752bd394dc74e5d311177f8dc475128abb
root <root@debian8.istic.univ-rennes1.fr>
2019-01-23 17:43:41 +0100

    add files from crash-test to dev
    Former-commit-id: a04fb9b496ceb44ecfecb0e3587264dafa7bfae9

---
+++ continueScriptBootKernel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $HOME/kdev
+qemu-system-x86_64 \
+  -kernel build/linux-x86-basic/arch/x86_64/boot/bzImage \
+  -initrd build/initramfs-busybox-x86.cpio.gz \
+  -nographic -append "console=ttyS0" > log.txt &
+
---
+++ scriptBootKernel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+TOP=$HOME/kdev
+cp -rf /TuxML/linux-4.13.3/ $TOP/linux
+
+#Configure Linux using simple defconfig and kvm options
+cd $TOP/linux
+make O=$TOP/build/linux-x86-basic x86_64_defconfig
+make O=$TOP/build/linux-x86-basic kvmconfig
+
+#Build Linux
+make O=$TOP/build/linux-x86-basic -j2
+
+#Boot with Qemu
+cd $TOP
+qemu-system-x86_64 \
+  -kernel build/linux-x86-basic/arch/x86_64/boot/bzImage \
+  -initrd build/initramfs-busybox-x86.cpio.gz \
+  -nographic -append "console=ttyS0" > log.txt &
+
+# Watch if Boot works or not
+cd ~/..
+./watchBootKernel.sh
