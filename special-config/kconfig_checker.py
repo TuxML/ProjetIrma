b7be3cee3d708034a44c4fbfd3673544d806e2e4
malocharo <poles.malo@gmail.com>
2019-01-27 15:08:40 +0100

    add tests for minimal conf and multiple kernel conf
    Former-commit-id: 7b9e7efd6c6fb7c96a28fade04f4f0df970769a5

--- special-config/kconfig_checker.py
+++ special-config/kconfig_checker.py
@@ -13,9 +13,16 @@ def get_linux_kernel(name, path=None):
     if name not in list_dir:
         print("Linux kernel not found, downloading...")
         wget_cmd = "wget https://cdn.kernel.org/pub/linux/kernel/v4.x/{}".format(name)
-        subprocess.call(wget_cmd, shell=True)
-        extract_cmd = "tar -xf {}".format(name)
-        subprocess.call(extract_cmd, shell=True)
+        try:
+            subprocess.call(wget_cmd, shell=True)
+        except OSError as err:
+            print(err)
+        extract_cmd = "tar -xvf {}".format(name)
+        try:
+            subprocess.call(extract_cmd, shell=True)
+        except OSError as err:
+            print(err)
+
     else:
         print("Linux kernel found.")
 
@@ -23,7 +30,7 @@ def get_linux_kernel(name, path=None):
 def generate_n_config(n, file):
     max = n
     if os.path.isfile(file):
-        cmd = "KCONFIG_ALLCONFIG=" + file +  " make randconfig"
+        cmd = "KCONFIG_ALLCONFIG=" + file + " make randconfig"
         print("compilation command", cmd, "in", os.getcwd())
         while n:
             status = subprocess.call(cmd, shell=True)
@@ -84,6 +91,7 @@ def check(n, file):
             n -= 1
     return report_data
 
+
 def display_error(nb_config_file, nb_error):
     if nb_error > 0:
         print("there is {} error in file config{}\n".format(nb_error, nb_config_file))
@@ -96,35 +104,66 @@ def generate_and_check(nrep, file_spe_options):
     return check(nrep, file_spe_options)
 
 
-if __name__ == '__main__':
-    get_linux_kernel("linux-4.13.3")
-    os.chdir("linux-4.13.3")
+def test_kernel(kernel_name, nrep, conf_file, expert_enable=0):
+    get_linux_kernel(kernel_name)
+    os.chdir(kernel_name)
     try:
         os.mkdir("gen_config")
-    except OSError as exc:  
+    except OSError as exc:
         if exc.errno == errno.EEXIST:
             pass
         else:
             raise
-    nrep=100
-    file_spe_options="../core/tuxml.config"
-    file_spe_options2= tempfile.NamedTemporaryFile(suffix=".config").name
-    with open(file_spe_options) as f:
-        lines = f.readlines()
-        with open(file_spe_options2, "w") as f1:
-            f1.writelines(lines)
-            f1.write('\n')
-            f1.write('CONFIG_EXPERT=y')
-    rep = generate_and_check(nrep, file_spe_options2)
-    #print(rep)
+    file_spe_options = conf_file
+    if expert_enable:
+        print("Test with CONFIG_EXPERT append at the end of the file")
+        file_spe_options2 = tempfile.NamedTemporaryFile(suffix=".config").name
+        with open(file_spe_options) as f:
+            lines = f.readlines()
+            with open(file_spe_options2, "w") as f1:
+                f1.writelines(lines)
+                f1.write('\n')
+                f1.write('CONFIG_EXPERT=y')
+        rep = generate_and_check(nrep, file_spe_options2)
+    else:
+        print("Test with CONFIG_EXPERT not at the end of the file, might (should) be still there because of dependances")
+        rep = generate_and_check(nrep, file_spe_options)
+    # print(rep)
     # if you only want to check, simply call check (see below)
     # rep = check(nrep, file_spe_options)
     # print(rep)
     uniq_opts = []
     for opt in rep['options']:
         if opt not in uniq_opts:
-            uniq_opts.append(opt)        
+            uniq_opts.append(opt)
     print(uniq_opts)
     nerrors = rep['nberrors'].sum()
+    print("Test on kernel {}".format(kernel_name))
     print((nerrors / len(rep)))
+    os.chdir("../..")
+
+
+
+def minimal_test(nrep, opt):
+    conf_file = os.getcwd() + "minimal.config"
+    with open(conf_file, "w+") as conf_mini:
+        conf_mini.write(opt)
+        conf_mini.close()
+    test_kernel("linux-4.13.3", nrep, conf_file)
+
+## for testing multiple kernel use kernel_list file
+## the kernel must be present on https://cdn.kernel.org/pub/linux/kernel/
+## for minimal invoke minimal_test method
+## pre-conf to generate & test 100 files
+## no clean up method exist yet
+
+if __name__ == '__main__':
+    with open("kernel_list") as kl:
+        a = [ln.strip() for ln in kl]
+        kl.close()
+    for x in a:
+        print(x)
+        test_kernel(x, 100, "../../core/tuxml.config")
+    minimal_test(100, "CONFIG_SLOB=y")
+
 
