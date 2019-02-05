a8fbfbbc84efbeb3e8d000cb5779c0b441e3f8db
PICARD Michaël <m.picard287@laposte.net>
2019-02-05 09:48:53 +0100

    WIP: Clean a lot of code about hardware details
    Former-commit-id: d78dd0a3c266fc285e6074dba120bf4598d8268d

--- compilation/environment.py
+++ compilation/environment.py
@@ -59,74 +59,84 @@ def __get_system_details():
     system = {
         "os": platform.system(),
         "distribution": platform.linux_distribution()[0],
-        "distribution_version": platform.linux_distribution()[1],
+        "distrib_version": platform.linux_distribution()[1],
         "os_kernel_version": platform.release()
     }
     return system
 
 
-## __get_type_of_disk
-# @author LE FLEM Erwan, MERZOUK Fahim, PICARD Michaël
+## __is_mechanical_disk
+# @author LE FLEM Erwan, MERZOUK Fahim, THOMAS Luis, PICARD Michaël
 # @version 2
-# @return 0 if the disk is a SSD, 1 if the disk is a regular HDD.
+# @brief Determine if TuxML project's content is located in a mechanical disk
+# @return False if the disk is a SSD, True if the disk is a regular HDD.
 # @todo Will the kernel will always be compiled in the same disk where tuxml scripts are located?
-def __get_type_of_disk():
+def __is_mechanical_disk():
     disk = psutil.disk_partitions()[0][0].split("/")[2]
     disk = ''.join(i for i in disk if not i.isdigit())
-    try:
-        result = subprocess.check_output(
-            ["cat", "/sys/block/{}/queue/rotational".format(disk)],
-            universal_newlines=True)
-    except subprocess.CalledProcessError:
-        return '-1'
-    return result.split('\n')[0].strip()
+    if os.path.exists("/sys/block/{}/queue/rotational".format(disk)):
+        with open("/sys/block/{}/queue/rotational".format(disk)) \
+                as disk_type_descriptor:
+            return bool(disk_type_descriptor.read(1))
+    else:
+        return False
+
+
+## __get_ram_size
+# @author PICARD Michaël
+# @version 1
+# @brief Retrieve the ram size in kB
+# @return a string corresponding to the size of the ram, in kB
+def __get_ram_size():
+    # The result is in Byte, so we divide by 1024 (2^10) in order to get it in
+    # kB.
+    return psutil.virtual_memory().total//1024
+
+
+## __get_cpu_freq()
+# @author PICARD Michaël
+# @version 1
+# @brief Retrieve and return the current frequencies of the 1st cpu
+def __get_cpu_freq():
+    # But for computation power, we have a problem : with cpuinfo, we get the
+    # actual computation power used right now, but this value change over time!
+    # It will probably be wiser to get the min and the max value?
+    frequencies = psutil.cpu_freq()
+    return int(frequencies.current)
+
+
+## __get_cpu_name()
+# @author PICARD Michaël
+# @version 2
+# @brief Retrieve and return the brand name of the cpu
+# @raises EnvironmentError if the cpu model name hasn't been retrieved
+def __get_cpu_name():
+    with open('/proc/cpuinfo') as cpuinfo:
+        for line in cpuinfo:
+            if line.startswith('model name'):
+                return line.split(':')[1].strip()
+        raise EnvironmentError("No cpu model name has been retrieved!")
 
 
 ## __get_hardware_details()
 # @author LE FLEM Erwan, PICARD Michaël
 # @version 2
 def __get_hardware_details():
-    # Ugly code but :
-    # -> Do we have a cleaner way to get model name and computation power of our
-    # cpu?
-    # For the name of our processor, it's enough, but other solution exist :
-    # (https://www.binarytides.com/linux-cpu-information/
-    # https://www.poftut.com/get-cpu-info-number-cpus-linux/)
-    # But for computation power, we have a problem : with cpuinfo, we get the
-    # actual computation power used right now, but this value change over time!
-    # It will probably be wiser to get the min and the max value?
-    # -> Same question for ram size?
-    with open('/proc/cpuinfo') as f:
-        for line in f:
-            if line.strip():
-                if line.rstrip('\n').startswith('model name'):
-                    cpu_name = line.rstrip('\n').split(':')[1].strip()
-                if line.rstrip('\n').startswith('cpu MHz'):
-                    cpu_freq = line.rstrip('\n').split(':')[1]
-                    cpu_freq = cpu_freq.split('.')[0].strip()
-
-        with open('/proc/meminfo') as f:
-            for line in f:
-                if line.strip():
-                    if line.rstrip('\n').startswith('MemTotal'):
-                        # Le deuxième strip élimite l'unité 'kB' de la chaîne
-                        memory = line.rstrip('\n').split(
-                            ':')[1].strip().split(' ')[0]
-
     hw = {
-        "cpu": cpu_name,
-        "cpu_freq": cpu_freq,
-        "ram": memory,
+        "cpu": __get_cpu_name(),
+        "cpu_freq": __get_cpu_freq(),
+        "ram": __get_ram_size(),
         "arch": platform.machine(),
-        "cpu_cores": str(os.cpu_count()),
-        "mechanical_drive": __get_type_of_disk()
+        "cpu_cores": os.cpu_count(),
+        "mechanical_drive": __is_mechanical_disk()
     }
 
     return hw
 
 
 ## get_environment_details
-# @author LEBRETON Mickaël
+# @author LEBRETON Mickaël, PICARD Michaël
+# @version 2
 def get_environment_details():
     env = {
         "system": __get_system_details(),
@@ -135,3 +145,22 @@ def get_environment_details():
     }
 
     return env
+
+
+## print_environment_details
+# @author LEBRETON Mickaël, PICARD Michaël
+# @version 2
+# @brief Using the print_method, pretty_print the environment details.
+def print_environment_details(environment_details, print_method=print):
+    for primary_key in environment_details:
+        print_method("    ==> {}".format(primary_key), flush=True)
+        for secondary_key in environment_details[primary_key]:
+            print_method(
+                "      --> {}: {}".format(
+                    secondary_key,
+                    environment_details[primary_key][secondary_key]
+                ), flush=True)
+
+
+if __name__ == "__main__":
+    print_environment_details(get_environment_details())
