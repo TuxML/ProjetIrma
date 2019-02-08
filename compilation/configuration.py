a2504f2d8c8ec2dabba9bc4a7d481256997ce797
PICARD Michaël <m.picard287@laposte.net>
2019-02-08 18:41:22 +0100

    WIP: Adding some configuration content
    Former-commit-id: 4905583da32f58aa01810c0c502df55b92e29c18

--- compilation/configuration.py
+++ compilation/configuration.py
@@ -1,10 +1,24 @@
 # @file configuration.py
 # @author PICARD Michaël
 
+import os
+
+
+def __get_compilation_mode(tiny, config):
+    if tiny:
+        return "tiny config"
+    elif config is not None:
+        return "given config"
+    else:
+        return "random config"
+
 
 def create_configuration(args):
     configuration = {
-
+        "core_used": os.cpu_count(),  # Temporary
+        "incremental_mod": args.incremental != 0,
+        "kernel_version_compilation": "4.13.3",
+        "config used": __get_compilation_mode(args.tiny, args.config)
     }
     return configuration
 
