73f8ba7c6451eb9d4df806adf698fa811fa1ca75
PICARD Michaël <m.picard287@laposte.net>
2019-02-26 21:58:39 +0100

    ENH, WIP: Adding dependencies autofix
    Former-commit-id: 056797a21babef6788b81c6f2b50f7b48b6065b5

--- compilation/PackageManager.py
+++ compilation/PackageManager.py
@@ -41,6 +41,7 @@ class PackageManager:
             "All the packages were found and installed.",
             color=COLOR_SUCCESS
         )
+        return True
 
     def __install_one_package(self, package):
         try:
@@ -57,9 +58,86 @@ class PackageManager:
             return False
 
     def fix_missing_dependencies(self, missing_files, missing_packages):
-        # TODO: translate tuxml_depman.build_dependencies
         new_packages = list()
-        return False
+
+        # Getting new package for each missing file.
+        for file in missing_files:
+            try:
+                output = subprocess.check_output(
+                    args="apt-file search {}".format(file),
+                    shell=True,
+                    universal_newlines=True,
+                    stderr=self.__logger.get_stderr_pipe()
+                )
+                lines = output.splitlines()
+
+                # We could have multiple package proposed. In this case, we
+                # select to install the first package who isn't already
+                # installed. Also, we have to be sure that the result is really
+                # the requested file.
+                package_found = False
+                for line in lines:
+                    package = line.split(":")[0]
+                    file_check = line.split(':')[1].split('/')[-1]
+                    if file_check != file:
+                        continue  # Not the right file
+                    if package in self.__package_list:
+                        continue  # Already installed
+                    # Because of the linux self management package dependencies,
+                    # we can't be 100% sure that our package is already here. So
+                    # we verify with a subprocess call.
+                    # A call returning 1 mean that the package isn't
+                    # installed.
+                    # Note that if it is already installed, we could have add it
+                    # to the __package_list, but we choose to don't do it,
+                    # because it will be present, whether we check it or not and
+                    # this could mess with the database. This behaviour could be
+                    # changed later.
+                    if subprocess.call(
+                        args="dpkg-query -l | grep {}".format(package),
+                        shell=True,
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL
+                    ):
+                        # Not need to add a package twice to the list of new
+                        # packages.
+                        if package not in new_packages:
+                            new_packages.append(package)
+                        package_found = True
+                        break
+                if not package_found:
+                    # Not the proper way, but we avoid recopying code with this.
+                    raise subprocess.CalledProcessError
+            except subprocess.CalledProcessError:
+                self.__logger.timed_print_output(
+                    "Unable to find the missing package for missing file : "
+                    "{}".format(file),
+                    color=COLOR_ERROR
+                )
+                return False
+
+        # Adding all the missing_packages if not already in the new_packages
+        # list.
+        # Note that we double check every package to be sure that it's not
+        # already installed and mean something corrupted occur.
+        for package in missing_packages:
+            if package in self.__package_list or not subprocess.call(
+                    args="dpkg-query -l | grep {}".format(file),
+                    shell=True,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL
+            ):
+                self.__logger.timed_print_output(
+                    "Fatal error : a package is called missing while he is "
+                    "already present!",
+                    color=COLOR_ERROR
+                )
+                return False
+            if package not in new_packages:
+                new_packages.append(package)
+
+        # We now install all the new_packages.
+        return self.install_package(new_packages)
 
     def get_package_list_copy(self):
         return self.__package_list.copy()
