362a87f9467b83fb9724aaa1a6c1e9231a88a217
PICARD Michaël <m.picard287@laposte.net>
2019-03-15 20:45:30 +0100

    TEST: Fix test configuration
    Former-commit-id: db284488d71fba808cc33bc423698fe7a097ffdf

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM tuxml/tartuxml:dev
-RUN tar xf /TuxML/linux-4.13.3.tar.xz -C /TuxML && rm /TuxML/linux-4.13.3.tar.xz
-RUN tar xf /TuxML/TuxML.tar.xz -C /TuxML && rm /TuxML/TuxML.tar.xz
-RUN apt-get install -qq -y --no-install-recommends $(cat /dependencies.txt)
-EXPOSE 80
-ENV NAME World
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -5,15 +5,16 @@ import compilation.configuration as configuration
 
 
 class DummyArgs:
-    def __init__(self, cpu_cores):
+    def __init__(self, cpu_cores, incremental):
         self.cpu_cores = cpu_cores
+	self.incremental = incremental
 
 
 def test_create_configuration():
     # We test if we have any throw, which should never happen
-    configuration.create_configuration(DummyArgs(0))
-    configuration.create_configuration(DummyArgs(-1000000))
-    configuration.create_configuration(DummyArgs(1000000))
+    configuration.create_configuration(DummyArgs(0,0))
+    configuration.create_configuration(DummyArgs(-1000000,0))
+    configuration.create_configuration(DummyArgs(1000000,0))
 
 
 def test_print_environment_details():
