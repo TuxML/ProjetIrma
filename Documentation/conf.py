3a8b9847db25f10b8c950cb8366b3b1a3e9f32e1
Georges Aaron RANDRIANAINA <randria-georges@outlook.com>
2020-06-26 14:59:48 +0200

    chores(Documentation/conf): set html_(short)_title variables
    Former-commit-id: 3a5af0c6a14e1d3375b918c2e427e700c9de948d

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -137,10 +137,10 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "TuxML Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = "TuxML Doc"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
